bab00b3c368fe4b20430b4871dbeba59f7ab7688
Nichole <ndeeb3@gatech.edu>
2019-02-23 16:00:41 -0500

    added bus and basic pizza shooter

---
+++ GT Bus Simulator 2019/Assets/Scripts/BusAbilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusAbilities : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Rigidbody bus;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Rigidbody clone;
+            clone = (Rigidbody) Instantiate(projectile, bus.position, projectile.rotation);
+            clone.velocity = transform.forward * 20;
+        }
+
+    }
+}
