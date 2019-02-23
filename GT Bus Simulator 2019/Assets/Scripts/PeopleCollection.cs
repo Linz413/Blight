ac27e97fe53208a27883340895f59b93c7496ab0
Nichole <ndeeb3@gatech.edu>
2019-02-23 12:44:10 -0500

    merge katie's branch

---
+++ GT Bus Simulator 2019/Assets/Scripts/PeopleCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PeopleCollection : MonoBehaviour
+{
+	public Text scoreText;
+	int score = 0;
+
+	void Start() {
+		scoreText.text = "Picked Up: " + score.ToString();
+	}
+    public void ReceivePickup() {
+    	score++;
+    	scoreText.text = "Picked Up: " + score.ToString();
+    }
+}
