50c60bad0bd0ef9d768b11f462b06ba754149d06
Lucas Liu <lucasliu@gatech.edu>
2019-04-19 21:52:23 -0400

    env physics update

---
+++ GT Bus Simulator 2019/Assets/collisionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public  float duration;
+    private bool active = false;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player") {
+            active = true;
+            rb.useGravity = true;
+        }
+           
+
+    }
+    private void FixedUpdate()
+    {
+        if (active)
+        {
+            duration -= Time.deltaTime;
+        }
+        if(duration <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }   
+    }
+}
